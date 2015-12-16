a01ba3bbdea42b011a82e382cfbc1021ddf22bb8
Justin Butkowski <jbutkowski@gmail.com>
2015-12-16 10:06:59 +1000

    WIP

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_key_base = ENV.fetch("RAILS_SECRET_KEY_BASE")
+Rails.application.config.secret_key_base = ad8483aef3a10ad335985d283e07b1ab24262789a824fb9afaf5016e8fe472dc2976b1c373b948ce46ad1b6148a67ff34f425c378690013521120c3342b757e
