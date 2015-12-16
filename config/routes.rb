89663622d1eed93787f80cc7f496ca5fba7aca86
Justin Butkowski <jbutkowski@gmail.com>
2015-12-16 10:45:50 +1000

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root "home#index"
+  root "notifications#index"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
